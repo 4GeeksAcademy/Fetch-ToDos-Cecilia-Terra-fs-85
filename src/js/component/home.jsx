eb60902d552557ccce4a5811a307ff9b27b1faec
Cecilia Terra <catreanden@gmail.com>
2024-12-02 18:30:10 +0000

    last commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,3 +1,4 @@
+import { func } from "prop-types";
 import React, { useState } from "react";
 
 
@@ -7,8 +8,7 @@ const Home = () => {
 	const [listaTarea, setListatarea] = useState([]);
 
 	function agregarTarea(e) {
-		// let arraynuevo = listaTarea.concat(tarea)
-		// setListatarea(arraynuevo)
+
 		if (e.key == "Enter") {
 			let arraynuevo = listaTarea.concat(tarea)
 			setListatarea(arraynuevo)
@@ -18,22 +18,31 @@ const Home = () => {
 
 	}
 
+	function borrarTarea(borrarItem) {
+		setListatarea(listaTarea.filter((item) => item !== borrarItem))
+
+	}
+
 	return (
 		<div className="text-center">
 			<h1>ToDos</h1>
 
-			<input type="text" onChange={(e) => setTarea(e.target.value)} value={tarea}
+			<input type="text" placeholder="wath do you need to do?" onChange={(e) => setTarea(e.target.value)} value={tarea}
 				onKeyDown={agregarTarea} />
 
 			<ul>
-				{listaTarea.map((tareas, index) => (
-					<li key={index}>{tareas}
+				{listaTarea.map((item, index) =>
+					<li key={index}>{item}  <i class="fa-solid fa-trash" onClick={() => borrarTarea(item)}></i>
+
 					</li>
 
-				))}
+				)}
 			</ul>
+
+			<div>Total task {listaTarea.length}</div>
 		</div>
 	);
 };
+
 
 export default Home;
